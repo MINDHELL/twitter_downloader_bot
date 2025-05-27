6b146db274edd7f923e238b9b7bc7952dae0c738
MINDHELL <Tgpurpose640@gmail.com>
2025-05-27 20:49:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # Your telegram bot token
-DEVELOPER_ID = 1234567890  # Your telegram id (used for error reporting and private mode)
-IS_BOT_PRIVATE = False  # Change this to True to make bot private
+BOT_TOKEN = "7769792227:AAFbg7gi7sUpCNT8hQHeZSiqmVs41Xlxbd8"  # Your telegram bot token
+DEVELOPER_ID = "6860316927"  # Your telegram id (used for error reporting and private mode)
+IS_BOT_PRIVATE = "False"  # Change this to True to make bot private
